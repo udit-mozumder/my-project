50d343df84a89ce8c5d9a46b3d44ac28dac80285
Udit Kumar Mozumder <udit.mozumder@zohomail.in>
2025-07-17 14:35:37 +0530

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -1,4 +1,9 @@
+import unittest
 from main import add
 
-def test_add():
-    assert add(2, 3) == 5
+class TestAddFunction(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(add(2, 3), 5)
+
+if __name__ == '__main__':
+    unittest.main()
